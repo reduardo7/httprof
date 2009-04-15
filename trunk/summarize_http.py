0f7163d77485db3b54896d042964f3914987bf22
snackypants <snackypants@12649128-1ef9-11de-b56b-c9554f25afe1>
2009-04-15 19:17:28 +0000

    Two new functions to help in describing HTTP responses.

--- trunk/summarize_http.py
+++ trunk/summarize_http.py
@@ -16,11 +16,59 @@ REQUEST_RESPONSE_MATCHER = re.compile(r"((GET|PUT|POST|HEAD|DELETE|PROPFIND|TRAC
 
 def summarize_http_connection(stream):
 
-      """stream: A stream of bytes, from either direction of a TCP
-connection. Returns an array of regular expression match objects from
-REQUEST_RESPONSE_MATCHER for each request or response found in stream."""
+      """stream: A stream of bytes. Returns an array of regular expression
+match objects from REQUEST_RESPONSE_MATCHER for each request or response
+found in stream."""
 
       return [ m for m in REQUEST_RESPONSE_MATCHER.finditer(stream) ]
+
+
+def get_response_segments(stream, matches):
+
+      """stream: A stream of bytes. matches: A sequence of regular
+expression match objects, such as returned by summarize_http_connection.
+Returns: a sequence of tuples describing the boundaries of the HTTP
+responses in stream."""
+
+      sgmnts = [ ]
+      for i in xrange(len(matches) - 1):
+            s = matches[i].start()
+            e = matches[i + 1].start()
+            if RESPONSE_MATCHER.match(stream[s:e]):
+                  sgmnts.append( (s, e) )
+
+      return sgmnts
+
+
+def get_response_body(stream, start, end):
+
+      """stream: A stream of bytes. start: The start of a response, NOT the
+start of the response body. end: The end of the response. start and end
+could come from get_response_segments, for example. Returns: The response
+body. If the response was compressed, the decompressed body is returned. The
+returned response body may be an empty string."""
+
+      from gzip import GzipFile
+      from StringIO import StringIO
+
+      try:
+            dlmtr = start + stream[start:end].index("\r\n\r\n")
+      except ValueError, e:
+            return ""
+
+      hdrs = { }
+      for h in stream[start:dlmtr].split("\r\n"):
+            try:
+                  k, v = h.split(":", 1)
+            except ValueError, e:
+                  hdrs[h] = None
+                  continue
+            hdrs[k.strip().lower()] = v.strip()
+
+      bdy = stream[dlmtr+4:end]
+      if "gzip" in hdrs.get("content-encoding", "").lower():
+            return GzipFile(fileobj=StringIO(bdy)).read()
+      return bdy
 
 
 if __name__ == "__main__":
